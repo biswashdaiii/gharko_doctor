0b8979e584cc1167ce74d577196f5e89f892758e
biswashdaiii <biswashghimire97@gmail.com>
2025-06-01 01:28:00 +0545

    theme done

---
+++ lib/theme/mytheme.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme(){
+  return ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 246, 245, 241),
+        fontFamily: "caramel"
+
+      );
+}
