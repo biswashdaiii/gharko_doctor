4b2c8f71fc807f76201566862ef7af5c2269146b
biswashdaiii <biswashghimire97@gmail.com>
2025-06-18 12:07:28 +0545

    user _repository

---
+++ lib/features/authentication/domain/repository/user_repository.dart
@@ -0,0 +1,14 @@
+import 'package:gharko_doctor/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:gharko_doctor/features/authentication/domain/entities/user_entity.dart';
+
+abstract interface class IUserRepository {
+  Future<Either<Failure, void>> registerUser(UserEntity student);
+
+  Future<Either<Failure, String>> loginUser(
+    String username,
+    String password,
+  );
+  UserEntity?getUser(String email);
+
+}
